2958ef13cb014531415b01370fce4818920d9dd1
ara-gone <72136410+ara-gone@users.noreply.github.com>
2023-08-01 00:34:59 +0100

    sprite assets from PokeAPI

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const quizContainer = document.getElementById('quiz');
 const titleContainer = document.getElementById('title');
 const startButton = document.getElementById('start');
+// const resetButton = document.getElementById('reset');
 
+// resetButton.addEventListener('click', resetPage);
 startButton.addEventListener('click', displayQuestion);
 
 function displayQuestion() {
@@ -10,8 +12,21 @@ function displayQuestion() {
 
     const questionElement = document.createElement('div');
     questionElement.className = 'button';
+    // g.setAttribute("id", "Div1");
     questionElement.innerHTML = 'this will be a question soon :)';
+    
+    // resetButton = document.createElement('div');
+    // resetButton.className = 'button';
+    // resetButton.setAttribute("id", "reset");
+    // resetButton.innerHTML = 'this will be a question soon :)';
 
     quizContainer.innerHTML = '';
     quizContainer.appendChild(questionElement);
+}
+
+function resetPage() {
+    quizContainer.style.display = 'none';
+
+    titleContainer.style.display = 'block';
+    startButton.style.display = 'block';
 }
