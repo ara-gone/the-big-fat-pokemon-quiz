a0d1cfe39b7efafe49f3cfecbcb760f98aa6c50b
ara-gone <72136410+ara-gone@users.noreply.github.com>
2023-08-07 12:58:37 +0100

    sprite spawning somewhere random

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const MAX_DEX_NUMBER = 898;
 const MAX_QUESTION_OPTIONS = 4;
+const MAX_QUIZ_LENGTH = 10;
 
 const quizContainer = document.getElementById('quiz');
 const animationContainer = document.getElementById('animation');
@@ -7,11 +8,58 @@ const titleContainer = document.getElementById('title');
 const startButton = document.getElementById('start');
 // const resetButton = document.getElementById('reset');
 
-let dexCounter = 0;
+let dexCounter = 0; // for chronological ordering of pokemon
+
+let questions = [];
+let currentQuestion = 0;
+
 let data = null;
+let userScore = {
+    Hardy:0,
+    Lonely:0,
+    Adamant:0,
+    Naughty:0,
+    Brave:0,
+    Bold:0,
+    Docile:0,
+    Impish:0,
+    Lax:0,
+    Relaxed:0,
+    Modest:0,
+    Mild:0,
+    Bashful:0,
+    Rash:0,
+    Quiet:0,
+    Calm:0,
+    Gentle:0,
+    Careful:0,
+    Quirky:0,
+    Sassy:0,
+    Timid:0,
+    Hasty:0,
+    Jolly:0,
+    Naive:0,
+    Serious:0
+};
 
 // resetButton.addEventListener('click', resetPage);
-startButton.addEventListener('click', displayQuestion);
+startButton.addEventListener('click', setupQuiz);
+
+function setupQuiz() {
+    // sortQuestions(chosenQuizLength);
+    sortQuestions();
+    console.log(questions);
+    displayQuestion();
+}
+
+function sortQuestions() {
+    for (let i = 0; i < MAX_QUIZ_LENGTH; i++) {
+        // TODO: ensure the same questions don't come up
+        questions.push(
+            data[Math.floor(Math.random() * data.length)]
+        );
+    }
+}
 
 function displayQuestion() {
 
@@ -20,7 +68,7 @@ function displayQuestion() {
 
     const questionElement = document.createElement('div');
     questionElement.className = 'question';
-    question_id = Math.floor(Math.random() * data.length);
+    let question_id = Math.floor(Math.random() * data.length);
     questionElement.innerHTML = data[question_id].question;
 
     const optionsElement = document.createElement('div');
@@ -85,10 +133,18 @@ function loopFunction(delay, callback){
 };
 
 function setup(){
-    animations = document.getElementsByClassName("animation");
-    for (let anim of animations) {
-        anim.style.backgroundImage = url_base + (Math.floor(Math.random() * 100) + 1) + ".png\"";
-    }
+    // animations = document.getElementsByClassName("animation");
+    // for (let anim of animations) {
+    //     anim.style.backgroundImage = url_base + (Math.floor(Math.random() * 100) + 1) + ".png\"";
+    // }
+
+    let img = document.createElement('img');
+    img.src = 'assets/sprites/icons/1.png';
+    img.class = 'sprites';
+    img.style.top = "60%";
+    // document.getElementById('body').appendChild(img);
+
+    // (Math.floor(Math.random() * 10) + 1)
 
     $.get("data/questions.csv", function(questions) {
          data = $.csv.toObjects(questions);
@@ -96,19 +152,21 @@ function setup(){
     });
 }
 
+// change this to function that resets image + position on page border hit
 loopFunction(20000, 
     function() {
         
         url_base = "assets/sprites/icons/";
         dexCounter++;
         if (dexCounter >= 1000) {
-            dexCounter = 0;
+            dexCounter = 0; // unused atm
         }
 
         animations = document.getElementsByClassName("sprites");
 
         for (let anim of animations) {
             anim.src = url_base + (Math.floor(Math.random() * MAX_DEX_NUMBER) + 1) + ".png";
+            anim.style.top = `${Math.floor(Math.random() * 100)}%`;
         }
     });
 
